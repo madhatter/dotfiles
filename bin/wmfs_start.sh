b3db1f151ce6cfe4bccad4f40b7c6379c6156067
Arvid Warnecke <madhatter@nostalgix.org>
2014-04-01 17:06:07 +0200

    Don't start mpd/mpdscribble in wmfs start script if it's already running.

--- bin/wmfs_start.sh
+++ bin/wmfs_start.sh
@@ -11,6 +11,6 @@ dropbox start&
 conky -c ~/.config/wmfs/scripts/conkyrc_top | while true; read line; do wmfs -c status "testbar $line"; done &
 conky -c ~/.config/wmfs/scripts/conkyrc_bottom | while true; read line; do wmfs -c status "bottom $line"; done &
 xmodmap ~/.Xmodmap &
-mpd &
-mpdscribble &
+pgrep -u "$EUID" -x mpd || mpd &
+pgrep -u "$EUID" -x mpdscribble || mpdscribble &
 exec dbus-launch wmfs
