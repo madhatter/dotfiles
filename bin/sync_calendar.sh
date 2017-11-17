f128be59bb5bc27bf19abce5aaca1ea4f1c06fe8
Arvid Warnecke <arvid.warnecke@otto.de>
2017-11-17 14:35:03 +0100

    Path changed in sync_calendar script.

--- bin/sync_calendar.sh
+++ bin/sync_calendar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker run -ti -v /tmp:/mnt/output -e USERNAME=${EWS_USERNAME} -e PASSWORD=${EWS_PASSWORD} madhatter/davmail
-scp /tmp/calendar.ics madhatter@nostalgix.org:/var/customers/webs/arvid/share/
+scp /tmp/calendar.ics madhatter@brutal.nostalgix.org:/var/www/share/
