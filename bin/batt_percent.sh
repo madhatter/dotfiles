d6e8266cbd39760310010c36f230634348fac6f4
Arvid Warnecke <madhatter@nostalgix.org>
2019-09-18 08:24:01 +0200

    Updated battery script.

--- bin/batt_percent.sh
+++ bin/batt_percent.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#LOAD=`cat /sys/class/power_supply/BAT0/energy_now`
-LOAD=`cat /sys/class/power_supply/BAT0/charge_now`
-#FULL=`cat /sys/class/power_supply/BAT0/energy_full`
-FULL=`cat /sys/class/power_supply/BAT0/charge_full`
-FULL_P=$(($FULL/100))
-echo $(($LOAD))
-echo $(($FULL))
-echo $(($LOAD / $FULL_P))
+LOAD=`cat /sys/class/power_supply/BAT0/energy_now`
+LOAD2=`cat /sys/class/power_supply/BAT1/energy_now`
+#LOAD=`cat /sys/class/power_supply/BAT0/charge_now`
+FULL=`cat /sys/class/power_supply/BAT0/energy_full`
+FULL2=`cat /sys/class/power_supply/BAT1/energy_full`
+#FULL=`cat /sys/class/power_supply/BAT0/charge_full`
+SFULL=$(($FULL + $FULL2))
+SLOAD=$(($LOAD + $LOAD2))
+
+FULL_P=$(($SFULL/100))
+echo $(($SLOAD))
+echo $(($SFULL))
+echo $(($SLOAD / $FULL_P))
