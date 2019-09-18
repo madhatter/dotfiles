edb2ea86364c99f266574230214c6ac200411e4f
Arvid Warnecke <madhatter@nostalgix.org>
2019-09-18 08:03:19 +0200

    Old keyboard layout config file and updated dwm start script to use another scrobbler and set location parameters to redshift.

--- bin/dwm_start.sh
+++ bin/dwm_start.sh
@@ -5,8 +5,9 @@ urxvtd -q -f -o &
 pgrep -u "$EUID" -x nm-applet || nm-applet --sm-disable &
 dropbox start &
 pgrep -u "$EUID" -x mpd || mpd &
-pgrep -u "$EUID" -x mpdscribble || mpdscribble &
-pgrep -u "$EUID" -x redshift || redshift &
+#pgrep -u "$EUID" -x mpdscribble || mpdscribble &
+pgrep -u "$EUID" -x mpdas || mpdas &
+pgrep -u "$EUID" -x redshift || redshift -l 53.35:10.459 &
 feh --bg-fill $HOME/wallpaper &
 #conky | while read -r; do xsetroot -name "$REPLY"; done &
 xmodmap $HOME/.Xmodmap &
