49c8e50dd87c4bedd5e12148c3b3787dc800859a
Arvid Warnecke <madhatter@nostalgix.org>
2019-12-06 09:20:59 +0100

    [x11] Cleanup of start script for dwm

--- bin/dwm_start.sh
+++ bin/dwm_start.sh
@@ -5,17 +5,14 @@ urxvtd -q -f -o &
 pgrep -u "$EUID" -x nm-applet || nm-applet --sm-disable &
 dropbox start &
 pgrep -u "$EUID" -x mpd || mpd &
-#pgrep -u "$EUID" -x mpdscribble || mpdscribble &
-pgrep -u "$EUID" -x mpdas || mpdas &
+pgrep -u "$EUID" -x mpdas || mpdas -d &
 pgrep -u "$EUID" -x redshift || redshift -l 53.35:10.459 &
-feh --bg-fill $HOME/wallpaper &
+feh --bg-scale $HOME/wallpaper/wallhaven-doom.png &
 #conky | while read -r; do xsetroot -name "$REPLY"; done &
 xmodmap $HOME/.Xmodmap &
-light-locker &
 dwmbar &
 wmname LG3D &
 dbus-update-activation-environment --systemd DISPLAY &
 echo $PATH > /tmp/path
-#exec dbus-launch dwm
 export _JAVA_AWT_WM_NONREPARENTING=1 &
 exec dwm
