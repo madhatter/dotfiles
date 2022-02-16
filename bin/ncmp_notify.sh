172e658752f89c019f2bae5843aea56f8a7a2a43
Arvid Warnecke <madhatter@nostalgix.org>
2022-02-16 17:17:57 +0100

    [ncmpcpp] Remove deprecated settings and add notification script.

--- bin/ncmp_notify.sh
+++ bin/ncmp_notify.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/bash
 
-# Get fields from mpc, split by tabs.
-IFS=$'\t' read album artist title \
-  <<< "$(mpc --format="%album%\t%artist%\t%title%")"
-
-notify-send --urgency=low --expire-time=5000 --app-name=ncmpcpp \
---icon=audio-x-generic "$title" "$artist" 
-#--icon=audio-x-generic "$title" "$artist\n$album" 
+notify-send "$(mpc current -f '[%title%]|Now Playing')" \
+    "$(mpc current -f '[[%artist%][ - %album%]]|[%file%]')" -i audio-speakers
