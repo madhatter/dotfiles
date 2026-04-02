58adc6a16dcba27b65a076f7317fc9eab6ac7f1d
Arvid Warnecke <madhatter@nostalgix.org>
2026-04-02 13:19:04 +0200

    [zsh] Added aliases for audio outputs for linux.

--- zsh/.zsh/linux.zsh
+++ zsh/.zsh/linux.zsh
@@ -9,3 +9,7 @@ FPATH=/usr/share/zsh/site-functions:~/.zsh/site-functions:$FPATH
 # MFA token generator. It reads the base32 secret from ~/.mfa/NAME.mfa and
 # copies the generated token to the clipboard.
 mfa() { oathtool --base32 --totp "$(cat ~/.mfa/$1.mfa)" | tee >(xclip -in -selection c) }
+
+# Aliases for quick audio output switching
+alias audio-samsung='pactl set-card-profile alsa_card.pci-0000_0b_00.1 output:hdmi-stereo'
+alias audio-m32up='pactl set-card-profile alsa_card.pci-0000_0b_00.1 output:hdmi-stereo-extra1'
